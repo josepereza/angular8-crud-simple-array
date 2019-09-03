b84eb63ec2d2605af52ba78d0a39c7c20f99efa5
rachid <olgor@hotmail.com>
2019-09-03 12:20:32 +0200

    logica acabada

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+import { Employee } from './models/employee';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  employeeArray: Employee[]=[
+    {id: 1, name: "Ryan", country: "Spain"}
+  ];
+  selectedEmployee: Employee = new Employee();
+
+  forEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length+1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    
+    this.selectedEmployee = new Employee(); //limpia el formulaario
+  }
+  delete(){
+    if(confirm('Are you sure you want to delete it???')){
+      this.employeeArray = this.employeeArray.filter(x=>x !=this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+    
+  }
+}
